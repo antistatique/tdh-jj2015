6064efb8497480d98cdbe67dd219566bc94ddc49
Toni Fisler <mail@tonifisler.ch>
2014-11-20 10:49:44 +0100

    speaker single page node

--- drupal/sites/all/themes/jj2015/template.php
+++ drupal/sites/all/themes/jj2015/template.php
@@ -4,3 +4,27 @@
  * @file
  * template.php
  */
+
+
+/**
+ * Overrides theme_menu_local_tasks().
+ */
+function jj2015_menu_local_tasks(&$variables) {
+  $output = '';
+
+  if (!empty($variables['primary'])) {
+    $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+    $variables['primary']['#prefix'] .= '<div class="clearfix"><ul class="tabs--primary nav nav-pills pull-right">';
+    $variables['primary']['#suffix'] = '</ul><div class="spacer spacer-xs"></div>';
+    $output .= drupal_render($variables['primary']);
+  }
+
+  if (!empty($variables['secondary'])) {
+    $variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+    $variables['secondary']['#prefix'] .= '<ul class="tabs--secondary pagination pagination-sm">';
+    $variables['secondary']['#suffix'] = '</ul>';
+    $output .= drupal_render($variables['secondary']);
+  }
+
+  return $output;
+}
